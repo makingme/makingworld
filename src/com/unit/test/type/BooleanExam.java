25a1689868efd0afe3b5cab370160a73030333e6
sam <sam@DESKTOP-K9ES1N0>
2020-03-04 01:55:46 +0900

    Git Test

---
+++ src/com/unit/test/type/BooleanExam.java
@@ -0,0 +1,14 @@
+package com.unit.test.type;
+
+public class BooleanExam {
+	public static boolean rTrue() {
+		return true;
+	}
+	public static boolean rFalse() {
+		return false;
+	}
+	public static void main(String[] args) {
+		System.out.println(rTrue());
+		
+	}
+}
